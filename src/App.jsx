f34d77005a763764ea88bac415eb3edefdaf438f
Caroline Berg <caroline.berg@no.experis.com>
2024-02-27 10:04:56 +0100

    'app' separated into components and passing prop 'name'

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,36 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="title">Welcome, {name}!</h1>
-      <p className="name-length">
-        Did you know that {name} is {name.length} characters long?!
-      </p>
-      <p className="name-reversed">
-        Also, {name} backwards is {reverse(name)}
-      </p>
+      <Title name={name}/>
+      <NameLength name={name}/>
+      <NameReversed name={name}/>
     </div>
   )
 }
 
 export default App
+
+function Title(props){
+  return(
+    // eslint-disable-next-line react/prop-types
+    <h1 className="title">Welcome, {props.name}!</h1>
+  )
+}
+
+function NameLength(props){
+  
+  return(
+    <p className="name-length">
+        Did you know that {props.name} is {props.name.length} characters long?!
+      </p>
+  )
+}
+
+function NameReversed(props){
+  
+  return(
+    <p className="name-reversed">
+        Also, {props.name} backwards is {reverse(props.name)}
+      </p>
+  )
+}
